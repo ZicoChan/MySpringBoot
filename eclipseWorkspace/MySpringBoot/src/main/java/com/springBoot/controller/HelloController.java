1fac60e3176ab6d45d72dc91b59b40dcfae5c2fe
ZicoChan <1041266134@qq.com>
2018-03-23 13:38:05 +0800

    swagger2-springBoot

--- eclipseWorkspace/MySpringBoot/src/main/java/com/springBoot/controller/HelloController.java
+++ eclipseWorkspace/MySpringBoot/src/main/java/com/springBoot/controller/HelloController.java
@@ -1,10 +1,8 @@
 package com.springBoot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -12,8 +10,9 @@ public class HelloController {
     
     @ApiOperation(value="测试接口")
     @GetMapping("/hello/test")
-    public String hello() {
-        return "hello,this is a springboot demo";
+    public String hello(String name) {
+        name = "hello " +name+ ", nice to meet you !";
+        return name;
     }
     
 }
